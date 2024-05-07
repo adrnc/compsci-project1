c2e64ac1741754a5bee44b44a20866e106ba6fda
adrnc <49120352+adrnc@users.noreply.github.com>
2024-05-07 14:29:38 +0200

    Implement facebook RAG

---
+++ playground/facebook-rag.py
@@ -0,0 +1,16 @@
+# type: ignore
+from transformers import RagTokenizer, RagRetriever, RagTokenForGeneration
+import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = 'True'
+
+tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-nq")
+retriever = RagRetriever.from_pretrained("facebook/rag-token-nq", index_name="exact", use_dummy_dataset=True)
+model = RagTokenForGeneration.from_pretrained("facebook/rag-token-nq", retriever=retriever)
+
+input_dict = tokenizer.prepare_seq2seq_batch("who holds the record in 100m freestyle", return_tensors="pt")
+
+generated = model.generate(input_ids=input_dict["input_ids"])
+print(tokenizer.batch_decode(generated, skip_special_tokens=True)[0])
+
+# should give michael phelps => sounds reasonable
