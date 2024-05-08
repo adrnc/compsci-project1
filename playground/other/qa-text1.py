9f631438ea99a09cafcbe378cea6273cc35d6301
adrnc <49120352+adrnc@users.noreply.github.com>
2024-05-08 19:02:43 +0200

    First QA test

---
+++ playground/other/qa-text1.py
@@ -0,0 +1,41 @@
+# type: ignore
+from transformers import pipeline, set_seed
+
+qa_model_name = "consciousAI/question-answering-roberta-base-s-v2"
+chat_model_name = "openai-community/gpt2"
+
+chat_max_length = 256
+chat_num_return_sequences = 1
+
+context = "🤗 Transformers is backed by the three most popular deep learning libraries — Jax, PyTorch and TensorFlow — with a seamless integration between them. It's straightforward to train your models with one before loading them for inference with the other."
+question = "Which deep learning libraries back 🤗 Transformers?"
+
+print(f"Context: {context}")
+print(f"Question: {question}")
+
+qa_model = pipeline("question-answering", model=qa_model_name)
+
+def encode_chat_question(question: str, context: str):
+    return f"User Question: {question}\n\nShort answer: {context}\n\nLong answer: "
+
+def decode_chat_answer(text: str, prompt: str):
+    return text[len(prompt):]
+
+qa_result = qa_model(question=question, context=context)
+qa_answer = qa_result["answer"]
+
+print(f"\nQA: {qa_answer}")
+
+chat_model = pipeline("text-generation", model=chat_model_name)
+
+set_seed(42)
+
+chat_prompt = encode_chat_question(question, qa_answer)
+chat_results = chat_model(chat_prompt, max_length=chat_max_length, truncation=False, num_return_sequences=chat_num_return_sequences)
+
+if not isinstance(chat_results, list):
+    chat_results = [chat_results]
+
+for chat_result in chat_results:
+    chat_text = chat_result["generated_text"]
+    print(f"Chat: {decode_chat_answer(chat_text, chat_prompt)}")
